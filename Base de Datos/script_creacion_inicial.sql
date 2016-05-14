d9e9a68ac12f6f219e4f359d2ba6634a3547bb17
felipecalvo <felipecalvo@yahoo.com.ar>
2016-05-14 15:51:45 -0300

    tabla rubros y esqueletos

--- Base de Datos/script_creacion_inicial.sql
+++ Base de Datos/script_creacion_inicial.sql
@@ -1,28 +1,63 @@
 CREATE SCHEMA SALUDOS
 
-
 CREATE TABLE SALUDOS.PUBLICACIONES(
-
-PUBL_CODIGO numeric(18,0) PRIMARY KEY,
-PUBL_DESCRIPCION nvarchar(255),
-PUBL_STOCK numeric (18,0),
-PUBL_INICIO date,
-PUBL_FINALIZACION date,
-PUBL_PRECIO numeric(18,2),
-PUBL_ESTADO varchar(10), --(borrador, activa, pausada, finalizada)
-PUBL_TIPO nvarchar(255),
-PUBL_PREGUNTAS bit,
-PUBL_FECHA date,
+	PUBL_CODIGO			numeric(18,0)	PRIMARY KEY,	--Publicacion_Cod
+	PUBL_DESCRIPCION	nvarchar(255),					--Publicacion_Descripcion
+	PUBL_STOCK			numeric (18,0),					--Publicacion_Stock
+	PUBL_INICIO			date,							--Publicacion_Fecha
+	PUBL_FINALIZACION	date,							--Publicacion_Fecha_Venc
+	PUBL_PRECIO			numeric(18,2),					--Publicacion_Precio
+	PUBL_ESTADO			varchar(10),					--(borrador, activa, pausada, finalizada)
+	PUBL_TIPO			nvarchar(255),					--Publicacion_Tipo
+	PUBL_PREGUNTAS		bit,							--new
+	PUBL_FECHA			date,							--???
 )
 
 CREATE TABLE SALUDOS.VISIBILIDADES(
-VISI_COD int IDENTITY, --si me responden y me dejan volar los códigos existentes 1000#
-VISI_COMISION_PUBLICACION numeric(18,  2),--maestra: Publicacion_Visibilidad_Precio
-VISI_COMISION_VENTA numeric(18,2), --maestra: Publicacion_Visibilidad_Porcentaje
-VISI_COMISION_ENVIO numeric(18, 2), --10% del valor inicial de la publicación.
-VISI_PERMITE_ENVIO bit,
-VISI_DESCRIPCION nvarchar(255),
+	VISI_COD					int				IDENTITY	PRIMARY KEY,	--reemplaza Publiacion_Visibilidad_Cod
+	VISI_COMISION_PUBLICACION	numeric(18,2),				--Publicacion_Visibilidad_Precio
+	VISI_COMISION_VENTA			numeric(18,2),				--Publicacion_Visibilidad_Porcentaje
+	VISI_COMISION_ENVIO			numeric(18,2),				--new. 10% del valor inicial de la publicación.
+	VISI_PERMITE_ENVIO			bit,						--new
+	VISI_DESCRIPCION			nvarchar(255),				--Publicacion_Visibilidad_Desc
 )
+
+CREATE TABLE SALUDOS.RUBROS(
+	RUBR_COD			int				IDENTITY	PRIMARY KEY,	--new
+	RUBR_NOMBRE			nvarchar(255),								--Publicacion_Rubro_Descripcion
+	RUBR_DESCRIPCION	nvarchar(255),								--new
+
+)
+
+--CREATE TABLE SALUDOS.TRANSACCIONES(
+--)
+
+--CREATE TABLE SALUDOS.CALIFICACIONES(
+--)
+
+--CREATE TABLE SALUDOS.EMPRESAS(
+--)
+
+--CREATE TABLE SALUDOS.CLIENTES(
+--)
+
+--CREATE TABLE SALUDOS.USUARIOS(
+--)
+
+--CREATE TABLE SALUDOS.FACTURAS(
+--)
+
+--CREATE TABLE SALUDOS.ROLES(
+--)
+
+--CREATE TABLE SALUDOS.FUNCIONALIDADES(
+--)
+
+--CREATE TABLE SALUDOS.ROLESXUSUARIO(
+--)
+
+--CREATE TABLE SALUDOS.FUNCIONALIDADESXROL(
+--)
 
 --A Publicaciones hay que agregarle los siguientes FK:
 --ALTER TABLE SALUDOS.PUBLICACIONES
