577c3c6fcfb5529b6123afc93e42bd99a4532e46
felipecalvo <felipecalvo@yahoo.com.ar>
2016-05-22 00:30:32 -0300

    generación de usernames y passwords

--- Base de Datos/script_creacion_inicial.sql
+++ Base de Datos/script_creacion_inicial.sql
@@ -1,3 +1,6 @@
+USE GD1C2016
+GO
+
 CREATE SCHEMA SALUDOS
 
 CREATE TABLE SALUDOS.PUBLICACIONES(
@@ -11,7 +14,7 @@ CREATE TABLE SALUDOS.PUBLICACIONES(
 	PUBL_TIPO			nvarchar(255),			--Publicacion_Tipo
 	PUBL_PREGUNTAS		bit,					--new
 	PUBL_PERMITE_ENVIO	bit,					--new
-	USUA_USERNAME		nvarchar(50),			--FK. Creador.
+	USUA_USERNAME		nvarchar(255),			--FK. Creador.
 	VISI_COD			int,					--FK. Visibilidad.
 	RUBR_COD			int,					--FK. Rubro.
 	CONSTRAINT PK_PUBLICACIONES PRIMARY KEY (PUBL_COD),
@@ -40,7 +43,7 @@ CREATE TABLE SALUDOS.TRANSACCIONES(
 	TRAN_PRECIO				numeric(18,2),	--Oferta_Monto (en caso de subasta). Sino, es el precio de compra.
 	TRAN_CANTIDAD_COMPRADA	numeric(2,0),	--Compra_Cantidad (en caso de compra directa)
 	TRAN_FECHA				datetime,		--Compra_Fecha u Oferta_Fecha. Momento de la transacción.
-	USUA_USERNAME			nvarchar(50),	--FK. Comprador/ofertante.
+	USUA_USERNAME			nvarchar(255),	--FK. Comprador/ofertante.
 	PUBL_COD				numeric(18,0),	--FK. Qué compra u oferta.
 	CONSTRAINT PK_TRANSACCIONES PRIMARY KEY (TRAN_COD),
 )
@@ -50,7 +53,7 @@ CREATE TABLE SALUDOS.CALIFICACIONES(
 	CALI_ESTRELLAS			numeric(18,0),	--Calificacion_Cant_Estrellas
 	CALI_DESCRIPCION		nvarchar(255),	--Calificacion_Descripcion
 	CALI_FECHA				datetime,		--new
-	USUA_USERNAME			nvarchar(50),	--FK. Quién califica.
+	USUA_USERNAME			nvarchar(255),	--FK. Quién califica.
 	PUBL_COD				numeric(18,0),	--FK. Respecto de qué publicación califica.
 	CONSTRAINT PK_CALIFICACIONES PRIMARY KEY (CALI_COD),
 )
@@ -69,7 +72,7 @@ CREATE TABLE SALUDOS.EMPRESAS(
 	EMPR_CODIGO_POSTAL		nvarchar(50),	--Publ_Empresa_Cod_Postal
 	EMPR_LOCALIDAD			nvarchar(50),	--new
 	EMPR_FECHA_CREACION		datetime,		--Publ_Empresa_Fecha_Creacion
-	USUA_USERNAME			nvarchar(50),	--FK. Usuario de la empresa.
+	USUA_USERNAME			nvarchar(255),	--FK. Usuario de la empresa.
 	RUBR_COD				int,			--FK. Rubro principal donde se desempeña.
 	CONSTRAINT PK_EMPRESAS PRIMARY KEY (EMPR_RAZON_SOCIAL, EMPR_CUIT)
 )
@@ -87,21 +90,21 @@ CREATE TABLE SALUDOS.CLIENTES(				--PARA EL QUE PUBLICA / PARA EL QUE COMPRA
 	CLIE_PISO				numeric(18,0),	--Publ_Cli_Piso		  / Cli_Piso
 	CLIE_LOCALIDAD			nvarchar(255),	--new
 	CLIE_NRO_DOCUMENTO		numeric(18,0),	--Publ_Cli_Dni		  / Cli_Dni
-	CLIE_TIPO_DOCUMENTO		nvarchar(50) DEFAULT 'DNI',--new
+	CLIE_TIPO_DOCUMENTO		nvarchar(50),	--new
 	CLIE_MAIL				nvarchar(50),	--Publ_Cli_Mail		  / Cli_Mail
-	USUA_USERNAME			nvarchar(50),	--FK. Usuario del cliente.
+	USUA_USERNAME			nvarchar(255),	--FK. Usuario del cliente.
 	CONSTRAINT PK_CLIENTES PRIMARY KEY (CLIE_NRO_DOCUMENTO, CLIE_TIPO_DOCUMENTO)
 )
 
 CREATE TABLE SALUDOS.USUARIOS(
-	USUA_USERNAME			nvarchar(50),	--new
-	USUA_PASSWORD			nvarchar(50),	--new
+	USUA_USERNAME			nvarchar(255),	--new
+	USUA_PASSWORD			nvarchar(255),	--new
 	USUA_NUEVO				bit,			--new
 	USUA_INTENTOS_LOGIN		tinyint,		--new
 	USUA_SIN_CALIFICAR		tinyint,		--new
-	USUA_TIPO				nvarchar(1),	--new. 'e' = empresa, 'u' = usuario
+	USUA_TIPO				nvarchar(1),	--new. 'e' = empresa, 'c' = cliente
 	USUA_HABILITADO			bit DEFAULT 1,
-	CONSTRAINT CK_USUARIO CHECK (USUA_TIPO IN ('e', 'u')),
+	CONSTRAINT CK_USUARIO CHECK (USUA_TIPO IN ('e', 'c')),
 	CONSTRAINT PK_USUA_USERNAME PRIMARY KEY (USUA_USERNAME)
 )
 
@@ -109,7 +112,7 @@ CREATE TABLE SALUDOS.FACTURAS(
 	FACT_COD				numeric(18,0) IDENTITY,	--Factura_Nro
 	FACT_FECHA				datetime,				--Factura_Fecha
 	FACT_TOTAL				numeric(18,2),			--Factura_Total
-	USUA_USERNAME			nvarchar(50),			--FK. A quién corresponde la factura.
+	USUA_USERNAME			nvarchar(255),			--FK. A quién corresponde la factura.
 	PUBL_COD				numeric(18,0),			--FK. Por qué publicación se factura.
 	CONSTRAINT PK_FACTURAS PRIMARY KEY (FACT_COD)
 )
@@ -135,7 +138,7 @@ CREATE TABLE SALUDOS.FUNCIONALIDADES(
 
 CREATE TABLE SALUDOS.ROLESXUSUARIO(
 	ROL_NOMBRE		nvarchar(50),
-	USUA_USERNAME	nvarchar(50),
+	USUA_USERNAME	nvarchar(255),
 	RXU_HABILITADO	bit DEFAULT 1,
 	CONSTRAINT PK_ROLESXUSUARIO PRIMARY KEY (ROL_NOMBRE, USUA_USERNAME)
 )
@@ -311,6 +314,7 @@ SELECT DISTINCT
 FROM gd_esquema.Maestra
 WHERE Publicacion_Rubro_Descripcion IS NOT NULL
 
+--Agrego visibilidades
 INSERT INTO SALUDOS.VISIBILIDADES(
 	VISI_COD, VISI_DESCRIPCION, VISI_COMISION_ENVIO,
 	VISI_COMISION_PUBLICACION, VISI_COMISION_VENTA)
@@ -318,3 +322,85 @@ SELECT DISTINCT
 	Publicacion_Visibilidad_Cod, Publicacion_Visibilidad_Desc, 0.10,
 	Publicacion_Visibilidad_Precio, Publicacion_Visibilidad_Porcentaje
 FROM gd_esquema.Maestra
+
+GO
+
+--Procedure que genera username y password para un cliente.
+CREATE PROCEDURE SALUDOS.generarUsuariosDeClientes
+	@dni nvarchar(255),
+	@nombre nvarchar(255),
+	@apellido nvarchar(255)
+AS
+	BEGIN
+		INSERT INTO SALUDOS.USUARIOS
+			(USUA_USERNAME, USUA_PASSWORD, USUA_TIPO)
+		VALUES
+			(LOWER(@nombre) + LOWER(@apellido), HASHBYTES('SHA2_256', @dni), 'c')
+	END
+GO
+
+--Procedure que genera username y password para una empresa.
+CREATE PROCEDURE SALUDOS.generarUsuariosDeEmpresas
+	@razonsocial nvarchar(255),
+	@cuit nvarchar(255)
+AS
+	BEGIN
+		INSERT INTO SALUDOS.USUARIOS
+			(USUA_USERNAME, USUA_PASSWORD, USUA_TIPO)
+		VALUES
+			(LOWER(@razonsocial), HASHBYTES('SHA2_256', @cuit), 'e')
+	END
+GO
+
+--Procedure que migra todos los clientes y empresas.
+CREATE PROCEDURE SALUDOS.migrarUsuarios
+AS
+	BEGIN
+		DECLARE cursorUsuariosDeClientes CURSOR FOR
+		SELECT CONVERT(nvarchar(255), CLIE_NRO_DOCUMENTO), CLIE_NOMBRE, CLIE_APELLIDO FROM SALUDOS.CLIENTES
+
+		DECLARE @dni nvarchar(255)
+		DECLARE @nombre nvarchar(255)
+		DECLARE @apellido nvarchar(255)
+
+		OPEN cursorUsuariosDeClientes
+		FETCH NEXT FROM cursorUsuariosDeClientes INTO @dni, @nombre, @apellido
+		WHILE (@@FETCH_STATUS = 0)
+	
+		BEGIN
+			EXECUTE SALUDOS.generarUsuariosDeClientes @dni, @nombre, @apellido
+			FETCH NEXT FROM cursorUsuariosDeClientes INTO @dni, @nombre, @apellido
+		END
+			
+		CLOSE cursorUsuariosDeClientes
+		DEALLOCATE cursorUsuariosDeClientes
+
+		-------------------------------------------
+		
+		DECLARE cursorUsuariosDeEmpresas CURSOR FOR
+		SELECT CONVERT(nvarchar(255), EMPR_RAZON_SOCIAL), EMPR_CUIT FROM SALUDOS.EMPRESAS
+
+		DECLARE @razonsocial nvarchar(255)
+		DECLARE @cuit nvarchar(255)
+
+		OPEN cursorUsuariosDeEmpresas
+		FETCH NEXT FROM cursorUsuariosDeEmpresas INTO @razonsocial, @cuit
+		WHILE (@@FETCH_STATUS = 0)
+	
+		BEGIN
+			EXECUTE SALUDOS.generarUsuariosDeEmpresas @razonsocial, @cuit
+			FETCH NEXT FROM cursorUsuariosDeEmpresas INTO @razonsocial, @cuit
+		END
+			
+		CLOSE cursorUsuariosDeEmpresas
+		DEALLOCATE cursorUsuariosDeEmpresas
+
+	END
+
+GO
+
+EXECUTE SALUDOS.migrarUsuarios
+GO
+
+UPDATE SALUDOS.USUARIOS
+SET USUA_NUEVO = 0, USUA_INTENTOS_LOGIN = 0
