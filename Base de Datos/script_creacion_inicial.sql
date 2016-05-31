2473e3b3d26fed14f7ef9e677d3d5406fccd4fd3
felipecalvo <felipecalvo@yahoo.com.ar>
2016-05-30 21:25:56 -0300

    migrando cuántas calificaciones debe cada usuario. spoiler alert: es cero

--- Base de Datos/script_creacion_inicial.sql
+++ Base de Datos/script_creacion_inicial.sql
@@ -591,6 +591,27 @@ WHERE TRAN_ADJUDICADA = 1
 	AND CALIFICACIONES.USUA_USERNAME = TRANSACCIONES.USUA_USERNAME
 
 
+--Agregando cuántas calificaciones tiene pendientes cada usuario.
+--Resulta que es cero para todos los usuarios, pero bue...
+--Hacer el select fue interesante. ¯\_(o.o)_/¯
+UPDATE SALUDOS.USUARIOS
+SET USUA_SIN_CALIFICAR = cuento.cuantos
+FROM(
+	SELECT usua.USUA_USERNAME, COUNT(*) cuantos
+	FROM SALUDOS.TRANSACCIONES trns, SALUDOS.PUBLICACIONES publ, SALUDOS.TIPOS tipo, SALUDOS.USUARIOS usua
+	WHERE	trns.PUBL_COD = publ.PUBL_COD AND
+			trns.TIPO_COD = tipo.TIPO_COD AND
+			trns.TRAN_ADJUDICADA = 1 AND
+			USUA_TIPO = 'Cliente' AND
+			trns.USUA_USERNAME = usua.USUA_USERNAME AND
+			NOT EXISTS(	SELECT *
+						FROM SALUDOS.CALIFICACIONES cali
+						WHERE	publ.PUBL_COD = cali.PUBL_COD AND
+								cali.USUA_USERNAME = trns.USUA_USERNAME)
+	GROUP BY usua.USUA_USERNAME) cuento
+WHERE cuento.USUA_USERNAME = SALUDOS.USUARIOS.USUA_USERNAME
+
+
 --Creación de tabla Fecha, function y procedure
 --para manejar la fecha del sistema.
 CREATE TABLE SALUDOS.FECHA(
