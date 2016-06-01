5c0461fccdb1f3a82e5768d0b0c1559865fc90da
Ivo Ursino <ursinoivo@gmail.com>
2016-06-01 18:51:23 -0300

    Avances en Listado Estadistico

---
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Dominio/Visibilidad.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Dominio
+{
+    class Visibilidad
+    {
+        public static List<String> getNombresVisibilidades()
+        {
+            List<String> visibilidades = new List<String>();
+            SqlDataReader reader;
+            SqlCommand consulta = new SqlCommand();
+            consulta.CommandType = CommandType.Text;
+            consulta.CommandText = "SELECT * from GD1C2016.SALUDOS.getNombresVisibilidades()";
+            consulta.Connection = Program.conexionDB();
+            reader = consulta.ExecuteReader();
+
+            while (reader.Read())
+                visibilidades.Add((String)reader.GetValue(0));
+
+            return visibilidades;
+        }
+    }
+}
--- Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Login/Login.cs
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Login/Login.cs
@@ -57,8 +57,8 @@ namespace WindowsFormsApplication1.Login
 
                 manager.ejecutarSP();
 
-                String tipoUsuario = Usuario.getTipoUsuario(username);
-                //String tipoUsuario = "Administrador";
+                //String tipoUsuario = Usuario.getTipoUsuario(username);
+                String tipoUsuario = "Administrador";
 
                 if (tipoUsuario.Equals("Administrador"))
                 {
