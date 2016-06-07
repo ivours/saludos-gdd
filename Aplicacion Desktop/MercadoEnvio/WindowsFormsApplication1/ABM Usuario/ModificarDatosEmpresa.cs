e40b7a080c728f5ac626d990ca3de77eef976034
Ivo Ursino <ursinoivo@gmail.com>
2016-06-07 03:45:44 -0300

    ABM Usuario - Validaciones modificacion empresa

--- Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/ABM Usuario/ModificarDatosEmpresa.cs
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/ABM Usuario/ModificarDatosEmpresa.cs
@@ -95,5 +95,154 @@ namespace WindowsFormsApplication1.ABM_Usuario
         {
             textBox13.Text = codigoPostal;
         }
+
+        private void validarCampos()
+        {
+            this.validarRazonSocial();
+            this.validarNombreDeContacto();
+            this.validarCuit();
+            this.validarEmail();
+            this.validarTelefono();
+            this.validarCiudad();
+            this.validarCalle();
+            this.validarNroCalle();
+            this.validarDepto();
+            this.validarLocalidad();
+            this.validarCodigoPostal();
+
+        }
+
+        //TODO: ver si puede incluir cualquier caracter o no
+        private void validarRazonSocial()
+        {
+            if (Validacion.estaVacio(textBox1.Text))
+                throw new Exception("Debe ingresar una razón social");
+
+            if (!Validacion.empiezaConCaracter(textBox1.Text))
+                throw new Exception("La razón social debe empezar con un caracter visible");
+        }
+
+        private void validarNombreDeContacto()
+        {
+            if (Validacion.estaVacio(textBox3.Text))
+                throw new Exception("Debe ingresar un nombre de contacto");
+
+            if (!Validacion.empiezaConCaracter(textBox3.Text))
+                throw new Exception("El nombre de contacto debe empezar con un caracter visible");
+
+            if (!Validacion.contieneSoloLetrasOEspacios(textBox3.Text))
+                throw new Exception("El nombre de contacto debe contener únicamente letras o espacios");
+        }
+
+        private void validarCuit()
+        {
+            if (Validacion.estaVacio(textBox4.Text))
+                throw new Exception("Debe ingresar un CUIT");
+
+            if (!Validacion.tieneFormatoDeCuit(textBox4.Text))
+                throw new Exception("El formato de CUIT ingresado no es válido. El CUIT debe estar compuesto por números y guiones");
+        }
+
+        private void validarEmail()
+        {
+            Extras.EmailChecker emailChecker = new Extras.EmailChecker();
+
+            if (Validacion.estaVacio(textBox6.Text))
+                throw new Exception("Debe ingresar un E-mail");
+
+            if (!emailChecker.IsValidEmail(textBox6.Text))
+                throw new Exception("El formato del E-mail no es válido. Por favor utilice un formato correcto");
+        }
+
+        private void validarTelefono()
+        {
+            if (Validacion.estaVacio(textBox7.Text))
+                throw new Exception("Debe ingresar un teléfono");
+
+            if (!Validacion.contieneSoloNumeros(textBox7.Text))
+                throw new Exception("El teléfono debe contener únicamente números");
+        }
+
+        private void validarCalle()
+        {
+            if (Validacion.estaVacio(textBox8.Text))
+                throw new Exception("Debe ingresar una calle");
+
+            if (!Validacion.empiezaConCaracter(textBox8.Text))
+                throw new Exception("La calle debe comenzar con un caracter visible");
+
+            if (!Validacion.contieneSoloLetrasOEspacios(textBox8.Text))
+                throw new Exception("La calle debe contener únicamente letras o espacios");
+        }
+
+        private void validarNroCalle()
+        {
+            if (Validacion.estaVacio(textBox9.Text))
+                throw new Exception("Debe ingresar un nro. de calle");
+
+            if (!Validacion.contieneSoloNumeros(textBox9.Text))
+                throw new Exception("El nro. de calle debe contener únicamente números");
+        }
+
+        private void validarDepto()
+        {
+            if (Validacion.estaVacio(textBox11.Text))
+                throw new Exception("Debe ingresar un depto.");
+
+            if (!Validacion.contieneSoloLetras(textBox11.Text))
+                throw new Exception("El depto. debe ser una letra");
+        }
+
+        private void validarLocalidad()
+        {
+            if (Validacion.estaVacio(textBox12.Text))
+                throw new Exception("Debe ingresar una localidad");
+
+            if (!Validacion.empiezaConCaracter(textBox12.Text))
+                throw new Exception("La localidad debe comenzar con un caracter visible");
+
+            if (!Validacion.contieneSoloLetrasOEspacios(textBox12.Text))
+                throw new Exception("La localidad debe contener únicamente letras o espacios");
+        }
+
+        private void validarCodigoPostal()
+        {
+            if (Validacion.estaVacio(textBox13.Text))
+                throw new Exception("Debe ingresar un código postal");
+
+            if (!Validacion.contieneSoloNumeros(textBox13.Text))
+                throw new Exception("El código postal debe contener únicamente números");
+
+            if (!textBox13.Text.Count().Equals(4))
+                throw new Exception("El código postal debe estar compuesto por 4 números");
+        }
+
+        private void validarCiudad()
+        {
+            if (Validacion.estaVacio(textBox2.Text))
+                throw new Exception("Debe ingresar una ciudad");
+
+            if (!Validacion.empiezaConCaracter(textBox2.Text))
+                throw new Exception("La ciudad debe comenzar con un caracter visible");
+
+            if (!Validacion.contieneSoloLetrasOEspacios(textBox2.Text))
+                throw new Exception("La ciudad debe contener únicamente letras o espacios");
+        }
+
+        private void limpiarCampos()
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox11.Clear();
+            textBox12.Clear();
+            textBox13.Clear();
+            numericUpDown1.Value = 0;
+        }
     }
 }
