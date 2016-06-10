73f47d3457d319bba6562597d337ee588a135764
felipecalvo <felipecalvo@yahoo.com.ar>
2016-06-10 16:05:59 -0300

    facturación de compras sin envío

--- Base de Datos/Facturas.sql
+++ Base de Datos/Facturas.sql
@@ -92,3 +92,41 @@ AS
 	)
 
 GO
+
+CREATE PROCEDURE SALUDOS.facturarCompra
+	@codPublicacion numeric(18,0),
+	@cantidadComprada numeric(18,0)
+AS
+	DECLARE @comisionVenta numeric(18,2)
+
+	SET @comisionVenta =	(SELECT VISI_COMISION_VENTA
+							FROM SALUDOS.VISIBILIDADES
+							WHERE VISI_COD = (	SELECT VISI_COD
+												FROM SALUDOS.PUBLICACIONES
+												WHERE PUBL_COD = @codPublicacion)
+											)
+	
+	INSERT INTO SALUDOS.FACTURAS(
+	FACT_FECHA, FACT_TOTAL, PUBL_COD, USUA_USERNAME)
+
+	VALUES(
+	saludos.fechaActual(), (@comisionVenta * @cantidadComprada), @codPublicacion,
+
+	(SELECT USUA_USERNAME
+	FROM SALUDOS.PUBLICACIONES
+	WHERE PUBL_COD = @codPublicacion)
+	)
+
+	DECLARE @codFactura numeric(18,0)
+	SET @codFactura = SCOPE_IDENTITY()
+
+	INSERT INTO SALUDOS.ITEMS(
+	ITEM_IMPORTE, ITEM_CANTIDAD,
+	ITEM_DESCRIPCION, FACT_COD)
+
+	VALUES(
+	@comisionVenta * @cantidadComprada, @cantidadComprada,
+	'Comisión por Venta', @codFactura
+	)
+
+GO
