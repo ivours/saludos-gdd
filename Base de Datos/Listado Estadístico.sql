863af863b187d5639ae54d06092a906cfc60840a
felipecalvo <felipecalvo@yahoo.com.ar>
2016-06-10 13:36:25 -0300

    considero sólo publicaciones finalizadas en vendedoresConMayorCantidadDeProductosNoVendidos y productosSinVenderDeUnVendedor

--- Base de Datos/Listado Estadístico.sql
+++ Base de Datos/Listado Estadístico.sql
@@ -16,6 +16,11 @@ RETURNS @tabla TABLE (Vendedor nvarchar(255), Productos_sin_vender int) AS
 		
 		SET @primerMes = (@trimestre * 3) - 2 
 		SET @tercerMes = @trimestre * 3
+
+		DECLARE @codFinalizada int
+		SET @codFinalizada = (	SELECT ESTA_COD
+								FROM SALUDOS.ESTADOS
+								WHERE ESTA_NOMBRE = 'Finalizada')
 			
 		INSERT @tabla
 			SELECT TOP 5 usua.USUA_USERNAME, COUNT(*) cantidad
@@ -23,6 +28,7 @@ RETURNS @tabla TABLE (Vendedor nvarchar(255), Productos_sin_vender int) AS
 			WHERE	usua.USUA_USERNAME = publ.USUA_USERNAME AND
 					publ.VISI_COD = visi.VISI_COD AND
 					VISI_DESCRIPCION = @visibilidad AND
+					ESTA_COD = @codFinalizada AND
 					YEAR(publ.PUBL_FINALIZACION) = @anio AND
 					(MONTH(publ.PUBL_FINALIZACION) BETWEEN @primerMes AND @tercerMes) AND
 					NOT EXISTS (	SELECT comp.PUBL_COD
@@ -45,17 +51,23 @@ RETURNS @tabla TABLE (	Código numeric(18,0), Descripción nvarchar(255), Precio n
 		SET @primerMes = (@trimestre * 3) - 2 
 		SET @tercerMes = @trimestre * 3
 
+		DECLARE @codFinalizada int
+		SET @codFinalizada = (	SELECT ESTA_COD
+								FROM SALUDOS.ESTADOS
+								WHERE ESTA_NOMBRE = 'Finalizada')
+
 		INSERT @tabla
 			SELECT PUBL_COD, PUBL_DESCRIPCION, PUBL_PRECIO, PUBL_INICIO, PUBL_FINALIZACION
 			FROM SALUDOS.PUBLICACIONES publ, SALUDOS.VISIBILIDADES visi
 			WHERE	USUA_USERNAME = @usuario AND
 					publ.VISI_COD = visi.VISI_COD AND
 					VISI_DESCRIPCION = @visibilidad AND
+					ESTA_COD = @codFinalizada AND
 					YEAR(publ.PUBL_FINALIZACION) = @anio AND
 					(MONTH(publ.PUBL_FINALIZACION) BETWEEN @primerMes AND @tercerMes) AND
 					NOT EXISTS (	SELECT comp.PUBL_COD
-								FROM SALUDOS.COMPRAS comp, SALUDOS.PUBLICACIONES publ2
-								WHERE publ2.PUBL_COD = comp.PUBL_COD AND publ2.PUBL_COD = publ.PUBL_COD)
+									FROM SALUDOS.COMPRAS comp, SALUDOS.PUBLICACIONES publ2
+									WHERE publ2.PUBL_COD = comp.PUBL_COD AND publ2.PUBL_COD = publ.PUBL_COD)
 			ORDER BY publ.PUBL_INICIO
 		RETURN;
 	END
