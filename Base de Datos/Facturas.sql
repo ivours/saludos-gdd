0d73e2a04981d6145f567ab31560b484fb2c58b4
felipecalvo <felipecalvo@yahoo.com.ar>
2016-06-10 16:20:06 -0300

    facturación de compras con envío

--- Base de Datos/Facturas.sql
+++ Base de Datos/Facturas.sql
@@ -98,13 +98,12 @@ CREATE PROCEDURE SALUDOS.facturarCompra
 	@cantidadComprada numeric(18,0)
 AS
 	DECLARE @comisionVenta numeric(18,2)
-
 	SET @comisionVenta =	(SELECT VISI_COMISION_VENTA
 							FROM SALUDOS.VISIBILIDADES
 							WHERE VISI_COD = (	SELECT VISI_COD
 												FROM SALUDOS.PUBLICACIONES
 												WHERE PUBL_COD = @codPublicacion)
-											)
+											 )
 	
 	INSERT INTO SALUDOS.FACTURAS(
 	FACT_FECHA, FACT_TOTAL, PUBL_COD, USUA_USERNAME)
@@ -127,6 +126,59 @@ AS
 	VALUES(
 	@comisionVenta * @cantidadComprada, @cantidadComprada,
 	'Comisión por Venta', @codFactura
+	)
+
+GO
+
+CREATE PROCEDURE SALUDOS.facturarCompraYEnvio
+	@codPublicacion numeric(18,0),
+	@cantidadComprada numeric(18,0)
+AS
+	DECLARE @comisionVenta numeric(18,2)
+	SET @comisionVenta =	(SELECT VISI_COMISION_VENTA
+							FROM SALUDOS.VISIBILIDADES
+							WHERE VISI_COD = (	SELECT VISI_COD
+												FROM SALUDOS.PUBLICACIONES
+												WHERE PUBL_COD = @codPublicacion)
+											 )
+	DECLARE @comisionEnvio numeric(18,2)
+	SET @comisionEnvio =	(SELECT VISI_COMISION_ENVIO
+							FROM SALUDOS.VISIBILIDADES
+							WHERE VISI_COD = (	SELECT VISI_COD
+												FROM SALUDOS.PUBLICACIONES
+												WHERE PUBL_COD = @codPublicacion)
+											 )
+
+	INSERT INTO SALUDOS.FACTURAS(
+	FACT_FECHA, FACT_TOTAL, PUBL_COD, USUA_USERNAME)
+
+	VALUES(
+	saludos.fechaActual(), (@comisionVenta * @cantidadComprada) + @comisionEnvio, @codPublicacion,
+
+	(SELECT USUA_USERNAME
+	FROM SALUDOS.PUBLICACIONES
+	WHERE PUBL_COD = @codPublicacion)
+	)
+
+	DECLARE @codFactura numeric(18,0)
+	SET @codFactura = SCOPE_IDENTITY()
+
+	INSERT INTO SALUDOS.ITEMS(
+	ITEM_IMPORTE, ITEM_CANTIDAD,
+	ITEM_DESCRIPCION, FACT_COD)
+
+	VALUES(
+	@comisionVenta * @cantidadComprada, @cantidadComprada,
+	'Comisión por Venta', @codFactura
+	)
+
+	INSERT INTO SALUDOS.ITEMS(
+	ITEM_IMPORTE, ITEM_CANTIDAD,
+	ITEM_DESCRIPCION, FACT_COD)
+
+	VALUES(
+	@comisionEnvio, 1,
+	'Comisión por Envío', @codFactura
 	)
 
 GO
