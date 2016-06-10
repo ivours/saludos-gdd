af2d72990705cac881e05acb406d937c9bb8f629
Ivo Ursino <ursinoivo@gmail.com>
2016-06-10 20:26:59 -0300

    Calificar - Listado de calificaciones pendientes. Agrego parametro loco en la db que hace que las consultas de funciones anden re piola (gracias Felipe)

---
+++ Aplicacion Desktop/MercadoEnvio/WindowsFormsApplication1/Calificar/OperacionesSinCalificar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.Calificar
+{
+    public partial class OperacionesSinCalificar : Form
+    {
+        String username;
+
+        public OperacionesSinCalificar(String username)
+        {
+            InitializeComponent();
+            this.username = username;
+            ConfiguradorDataGrid.configurar(dataGridView1);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            ConfiguradorDataGrid.llenarDataGridConConsulta(this.califificacionesPendientes(), dataGridView1);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int cantidadCalificacionesPendientes()
+        {
+            SqlDataReader reader;
+            SqlCommand consulta = new SqlCommand();
+            consulta.CommandType = CommandType.Text;
+            consulta.CommandText = "SELECT GD1C2016.SALUDOS.cantidadCalificacionesPendientes(@usuario)";
+            consulta.Parameters.Add(new SqlParameter("@usuario", this.username));
+            consulta.Connection = Program.conexionDB();
+            reader = consulta.ExecuteReader();
+            reader.Read();
+
+            return (int)reader.GetValue(0);
+        }
+
+        private SqlDataReader califificacionesPendientes()
+        {
+            SqlDataReader reader;
+            SqlCommand consulta = new SqlCommand();
+            consulta.CommandType = CommandType.Text;
+            consulta.CommandText = "SELECT * from GD1C2016.SALUDOS.calificacionesPendientes(@usuario)";
+            consulta.Parameters.Add(new SqlParameter("@usuario", this.username));
+            consulta.Connection = Program.conexionDB();
+            reader = consulta.ExecuteReader();
+
+            return reader;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
